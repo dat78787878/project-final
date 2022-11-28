4f7812b1c2df4272bacd12fe28a02833622479c0
dat78787878 <mai.nam.nghia.bunbu@gmail.com>
2022-11-28 16:09:26 +0700

    check url

--- front/apps/admin/src/utils/getbaseUrl.ts
+++ front/apps/admin/src/utils/getbaseUrl.ts
@@ -2,14 +2,17 @@ let url: string | undefined = '';
 
 if (process.env['NODE_ENV'] === 'development') {
   url = 'http://52.194.87.144:8000/';
+  console.log("url1",url)
 } else {
   if (
     process.env['NX_DOMAIN_PROD'] &&
     window.location.hostname.includes(process.env['NX_DOMAIN_PROD'])
   ) {
     url = process.env['NX_URL_PROD'];
+    console.log("url2",url)
   } else {
     url = process.env['NX_URL_STAGING'];
+    console.log("url3",url)
   }
 }
 console.log("url",url)
