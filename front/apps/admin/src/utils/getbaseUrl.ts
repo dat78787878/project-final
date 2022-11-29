75471e084bd3ab2bdb551b81a5599e23306bd595
dat78787878 <mai.nam.nghia.bunbu@gmail.com>
2022-11-29 13:51:47 +0700

    test

--- front/apps/admin/src/utils/getbaseUrl.ts
+++ front/apps/admin/src/utils/getbaseUrl.ts
@@ -1,19 +1,21 @@
 let url: string | undefined = '';
 
 if (process.env['NODE_ENV'] === 'development') {
-  url = 'http://api.do-thanh-dat.tech:80/';
- 
+  url = 'https://api.do-thanh-dat.tech/';
+  console.log("1")
 } else {
   if (
     process.env['NX_DOMAIN_PROD'] &&
     window.location.hostname.includes(process.env['NX_DOMAIN_PROD'])
   ) {
     url = process.env['NX_URL_PROD'];
+    console.log("2")
    
   } else {
     url = process.env['NX_URL_STAGING'];
+    console.log("3")
    
   }
 }
-
+console.log("url",url)
 export const baseUrl = url;
